fd2c501189f30f63700e9d2954920d733e807f00
karinakaren <161085384+karinakaren@users.noreply.github.com>
2024-02-27 17:05:59 -0300

    Add files via upload

--- src/js/menu-hamburger.js
+++ src/js/menu-hamburger.js
@@ -1,13 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menuItems = document.querySelector('.menu-items');
-  
-    menuToggle.addEventListener('click', function() {
-      // Alternar a classe 'active' nos itens do menu
-      menuItems.classList.toggle('active');
-  
-      // Alternar entre ícone de hamburger e ícone de "x"
-      menuToggle.querySelector('.hamburger').classList.toggle('close');
-    });
-  });
-  
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+  document.querySelector('.menu').classList.toggle('active');
+});
